d1c0cd6af7009df69b44c2a0b141c06d5f0c1580
Abdelrhman Bassem <abdob5471@gmail.com>
2026-08-08 17:14:43 +0300

    Update change_over_time.sql

--- scripts/change_over_time.sql
+++ scripts/change_over_time.sql
@@ -12,17 +12,38 @@ SQL Functions Used:
     - Aggregate Functions: SUM(), COUNT(), AVG()
 ===============================================================================
 */
---Analyze sales performance over time
-SELECT 
-	t.Year_Of_Order,
-	t.Month_Of_Order,
-	SUM(t.TotalOrderByTime) OVER(ORDER BY t.Year_Of_Order ,t.Month_Of_Order) Total_Order_OVER_Time
-FROM(
+
+-- Analyse sales performance over time
+-- Quick Date Functions
 SELECT
-	YEAR(order_date) Year_Of_Order,
-	MONTH(order_date) Month_Of_Order,
-	SUM(Price) TotalOrderByTime
-FROM Gold.fact_sales
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
 WHERE order_date IS NOT NULL
-GROUP BY YEAR(order_date),MONTH(order_date)
-)t
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+-- DATETRUNC()
+SELECT
+    DATETRUNC(month, order_date) AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date);
+
+-- FORMAT()
+SELECT
+    FORMAT(order_date, 'yyyy-MMM') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM');
